640e40565814bf9efa2fdf65998d6b79d0522322
Andreas Lucas <andreas@luccon.net>
2021-10-21 14:49:35 +0000

    script version 20211021

--- ElevateUser.ps1
+++ ElevateUser.ps1
@@ -1,48 +1,78 @@
 <#
+Script Info
 
+Author: Andreas Lucas [MSFT]
+Download: 
+
+Disclaimer:
+This sample script is not supported under any Microsoft standard support program or service. 
+The sample script is provided AS IS without warranty of any kind. Microsoft further disclaims 
+all implied warranties including, without limitation, any implied warranties of merchantability 
+or of fitness for a particular purpose. The entire risk arising out of the use or performance of 
+the sample scripts and documentation remains with you. In no event shall Microsoft, its authors, 
+or anyone else involved in the creation, production, or delivery of the scripts be liable for any 
+damages whatsoever (including, without limitation, damages for loss of business profits, business 
+interruption, loss of business information, or other pecuniary loss) arising out of the use of or 
+inability to use the sample scripts or documentation, even if Microsoft has been advised of the 
+possibility of such damages
 #>
-param($eventRecordID,$eventChannel)
+<#
+Event ID's
+2000 Error Configuration file missing
+2001
+2002
+2003
+2004 Information The user is already user of this group. TTL will be updated
+2005 Inf
+#>
+param(
+    $eventRecordID,
+    $eventChannel,
+    [Parameter (Mandatory=$false)]
+    $ConfigurationFile
+    )
 
-
-#    [Parameter (Mandatory=$false)]
-    $ConfigurationFile="C:\Program Files\WindowsPowershell\Scripts\JIT.config"
-
+if ($ConfigurationFile -eq $null)
+{
+    $ConfigurationFile = (Get-Location).Path + '\JIT.config'
+}
 
 if (!(Test-Path $ConfigurationFile))
 {
-    Write-EventLog -LogName 'Tier 1 Management' -Source 'T1Mgmt' -EventId 1 -EntryType Error -Message "Configuratin file missing $configurationFile"
+    Write-EventLog -LogName 'Tier 2000 Management' -Source 'T1Mgmt' -EventId 1 -EntryType Error -Message "Configuration file missing $configurationFile"
+    return
 }
 $config = Get-Content $ConfigurationFile | ConvertFrom-Json
 $eventLog = $config.EventLog
 $ElevateEventID = $config.ElevateEventID
 $Event = Get-WinEvent  -LogName $eventLog     -FilterXPath "<QueryList><Query Id='$ElevateEventID' Path='$eventLog'><Select Path='$eventLog'>*[System[(EventRecordID='$eventRecordID')]]</Select></Query></QueryList>"
-#$event = Get-winevent -LogName $eventChannel -FilterXPath "<QueryList><Query Id='0'               Path='$eventChannel'><Select Path='$eventChannel'>*[System [(EventRecordID=$eventRecordID)]]</Select></Query></QueryList>"
 $Request = ConvertFrom-Json $Event.Message
 $ServerGroupName = $Request.ServerGroup
 $ServerDomain = $Request.ServerDomain
 $UserDN = $Request.UserDN
 $TTL = $Request.ElevationTime
-$AdminGroup = Get-ADGroup -Filter {SamAccountName -eq $ServerGroupName} -Server $ServerDomain
-$User = Get-ADuser -Filter {DistinguishedName -eq $UserDN} -Properties MemberOf
+$AdminGroup = Get-ADGroup -Filter {SamAccountName -eq $ServerGroupName} -Server $config.Domain
+$User = Get-ADuser -Filter {DistinguishedName -eq $UserDN} -Properties MemberOf -Server $config.Domain
 if ($AdminGroup -eq $null)
 {
-    Write-EventLog -Source $config.EventLog -EventId 1000 -EntryType Error -Message "Invalid group in request $config"
+    Write-EventLog -Source $config.EventLog -EventId 2001 -EntryType Error -Message "Can not find $ServerGroupName"
     return
 }
 
 if ($User -eq $null) 
 {
-    Write-EventLog -Source $config.EventLog -EventId 1100 -EntryType Error -Message "Invalid user in request $config"
+    Write-EventLog -Source $config.EventLog -EventId 2002 -EntryType Error -Message "Invalid user in request $config"
     return
 }
 if ($TTL -gt $config.MaxElevatedTime)
 {
-    Write-EventLog -Source $config.EventLog -EventId 1200 -EntryType Error -Message "Invalid elevation time in request $config"
+    Write-EventLog -Source $config.EventSource -EventId 2003 -EntryType Error -Message "Invalid elevation time in request $config"
     return
 }
 if ($user.MemberOf -contains $AdminGroup)
 {
-    Remove-ADGroupMember $AdminGroup -Members $User.DistinguishedName
+    Write-EventLog -LogName $config.EventLog -Source $config.EventSource -EntryType Information -EventId 2004 -Message "$($user.SamAccountName) is member of $AdminGroup"
+    Remove-ADGroupMember $AdminGroup -Members $User.DistinguishedName -Confirm:$false
 }
 Add-ADGroupMember -Identity $AdminGroup -Members $User -MemberTimeToLive (New-TimeSpan -Minutes $TTL)
 Write-EventLog -Source $config.EventSource -LogName $config.EventLog -EventId 101 -EntryType Information -Message "User added to group $config"
