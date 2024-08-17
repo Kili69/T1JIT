87ae94621da7a2b178968f839a049c6670e40118
Andreas Lucas (aka kili) <andreas.lucas@outlook.com>
2024-08-17 11:43:36 +0200

    Version 20240817 documentation update

--- modules/0.1/just-in-time-request.psm1
+++ modules/0.1/just-in-time-request.psm1
@@ -19,6 +19,12 @@ possibility of such damages
 This module file contains the user functions to request the administrator privileges
 #>
 
+#region global variables
+[int]$_configBuildVersion = "20231108"
+$GlobalCatalogServer = "$((Get-ADDomainController -Discover -Service GlobalCatalog).HostName):3268"
+
+#endregion
+
 function Write-ScriptMessage {
     param (
         [Parameter (Mandatory, Position=0)]
@@ -41,11 +47,38 @@ function Write-ScriptMessage {
     }
 }
 <#
-Import the configuration from JIT.config
+.SYNOPSIS
+    Import the configuration from JIT.config file
+.DESCRIPTION
+    Reading the JIT.config from the System variable or a expicite JIT configuration file. 
+    The function return the configuration as JIT config object
+    This is a module private function
+.PARAMETER configurationFile
+    this is a optional parameter to use a dedicated configuration file.
+    If this parameter is not available the function read the configuration files
+    from the in the $env:JustInTimeConfig varaible or from the current directory
+.INPUTS
+    The path to the configuration file as string
+.OUTPUTS
+    JIT config object as PSObject 
+.EXAMPLE
+    Get-JITConfig
+    Tries to read the JIT configuration from the path in the SYSTEM variable JustInTimeConfig. 
+    If the environement is not available or the file doesn't exist, the function tries to read 
+    the configuration file from the current directory
+    Get-JITConfig .\jit.config
+        Read the configuration from the path
+    Get-JITConfig -ConfigurationFile .\jit.config
+        Read the configuration from the path
 #>
 function Get-JITconfig{
-    [int]$_configBuildVersion = "20231108"
-    #Reading and validating configuration file
+    param(
+        [Parameter (Mandatory=$false, Position=0)]
+        [string]$configurationFile
+    )
+    #region parameter validation
+    #If the parameter configurationFile is null or empty, change the variable to the value of
+    #the system environment JustInTimeConfig 
     if ($null -eq $configurationFile){
         if ($env:JustInTimeConfig -eq ""){
             $configurationFile = ".\jit.config"
@@ -59,6 +92,7 @@ function Get-JITconfig{
 
         }
     }
+    #endregion
     if (!(Test-Path $configurationFile))
     {
         throw "Configuration $configurationFile missing"
@@ -83,7 +117,30 @@ function Get-JITconfig{
 }
 
 <#
-Searching the user object in the entire forest with the user PAC
+.SYNOPSIS
+    Searching the user object in the entire forest with the user PAC
+.DESCRIPTION
+    This function searches a user in the entire forest and add the all group membership
+    SID as a hashtable to the object
+.PARAMETER USER
+    If the name of the user. The username can be in the UPN or Domain\Name format. if the
+    domain name is not part of the parameter, the user will be searched in the current domain
+.INPUTS
+    The name of the user
+.OUTPUTS
+    ActiveDirectoy.ADUser object
+.EXAMPLE
+    Get-User
+        return the current user object
+.EXAMPLE 
+    Get-User myuser@contoso.com
+        searches for the user with the user principal name myuser@contoso.com in the forest
+.EXAMPLE
+    Get-User contos\myuser
+        searches for the user myuser in the contos domain
+.EXAMPLE
+    Get-User myuser
+        searches for the user myuser in the current domain
 #>
 function Get-User{
     param(
@@ -92,15 +149,19 @@ function Get-User{
         [string]
         $User
     )
-    $GlobalCatalogServer = "$((Get-ADDomainController -Discover -Service GlobalCatalog).HostName):3268"
+    #determine the user parameter format. The function support the format UPN, Domain\UserName, UserName
     switch ($user){
         ""{
+            #searching for the current user object in AD
             $oUser = get-ADuser $env:UserName -Properties ObjectSID  
         }
         ({$_ -like "*@*"}){
+            #searching for the user in the UPN format
             $oUser = get-ADUser -Filter "UserPrincipalName -eq '$User'" -Server $GlobalCatalogServer -Properties ObjectSID
         }
         ({$_ -like "*\*"}){
+            #searching for the user in a specified domain
+            #enumerate all domains in the forest and compare the domain netbios name with the parameter domain
             foreach ($DomainDNS in (GEt-ADForest).Domains){
                 $Domain == Get-ADDomain -Server $DomainDNS
                 if ($Domain.NetBIOSName -eq $user.split("\")[0]){
@@ -113,13 +174,52 @@ function Get-User{
             $oUser = Get-aduser -Filter "SamAccountName -eq '$User'"
         }
     }
+    #To enumerate the recursive memberof SID of the user a 2nd LDAP query is needed. The recursive memberof SID stored in the TokenGroups 
+    # attribute
+    #extrating the domain component from the user distinguishedname
     $UserDomainDN = [regex]::Match($oUser.DistinguishedName,"DC=.*").value
+    #enumerating the Domain DNS name from the user distinguished name
     $UserDomainDNSName = (Get-ADForest).domains | Where-Object {(Get-ADDomain -Server $_).DistinguishedName -eq $UserDomainDN}
+    #searching the user with the TokenGroups attribute
     $oUser = Get-ADUser -LDAPFilter "(ObjectClass=user)" -SearchBase $ouser.DistinguishedName -SearchScope Base -Server $userDomainDNSName -Properties "TokenGroups"
     return $oUser
 }
 
 #region Exported functions
+<#
+New-JITRequestAdminRequest
+.SYNOPSIS
+    requesting administrator privileges to a server
+.DESCRIPTION
+    The New-JITRequestAdminAccess creates a new Event to request administrator privileges on a server.
+    This function validates the parameters and create the required event log entry
+.PARAMETER Server
+    Is the name of the server. The server can be in the format hostname or FQDN. This parameter is mandatory
+.PARAMETER Minutes
+    Is the requested a mount of administrator time in minutes. If the parameter is 0 or empty the configured
+    default value time will be used. The parameter cannot exceed the maximum elevation time. If the parameter
+    is greater the configured maximum elevation time, the time will be reduced to the maximum elevation time
+.PARAMETER User
+    This parameter is used if the request is for a different user then the calling user
+.PARAMETER UIused
+    This is a optional parameter to use the output for the PS GUID. If this parameter is $false (Default value)
+    the output will formated
+.INPUTS
+    The name of the server on postion 0
+    the amount of minutes on position 1
+.OUTPUTS
+    None
+.EXAMPLE
+    New-JITRequestAdminAccess myhost.contoso.com
+        Create a administrator request for the current user for server myhost.compunter.com
+    New-JetRequestAdminAccess myhost
+        Create a administrator request for the current user for the server myhost. My host must exists
+        in the forest necessarily in the current domain
+    New-JITRequestAdminAccess myhost.contoso.com 30
+        Request administrator privileges for myhost.contoso.com for 30 minutes
+    New-JITRequestAdminAccess -Server myhost.contoso.com -Minutes 30 -user myuser@contoso.com
+        Request administrator privileges for myhost.contoso.com for 30 minutes for user myuser@contoso.com
+#>
 function New-JITRequestAdminAccess{
     param(
         # The name of the server requesting administrator privileges
@@ -137,9 +237,12 @@ function New-JITRequestAdminAccess{
         [Parameter (Mandatory = $false)]
         [bool] $UIused = $false
     )
+    #reading the current configuration
     $config = Get-JITconfig
-    $GlobalCatalogServer = "$((Get-ADDomainController -Discover -Service GlobalCatalog).HostName):3268"
     #region validation of minutes
+    #if the vlaue must be between 5 and the maximum configured value. If the value is lower then 5
+    #then the minutes variable will be set to 5
+    #if the parameter is 0 the parameter will be changed to the configured default value 
     switch ($Minutes) {
         0 {
             $Minutes = $config.DefaultElevatedTime
@@ -153,18 +256,24 @@ function New-JITRequestAdminAccess{
     }
     #endregion
     #region user elvalutation
+    #terminate the function of the user object is not available in the AD forest
    $oUser = Get-User $User
     if ($Null -eq $oUser){
         Write-ScriptMessage "Can find the user object." -Severity Warning -UIused $UIused
         exit
     }
     #endregion
+    #if the server variable contains a . the hostname is FQDN. The function searches for the computer
+    #object with this DNSHostName attribute. This function does not query the DNS it self. It is mandatory
+    #the primary DNS name is registered.
+    #if the server parameter is not as FQDN the function searches for the computername in the AD forest
+    #If mulitple computers with the same name exists if the forest the function return a $null object
     if ($server -like "*.*"){
         $oServer = Get-ADcomputer -Filter "DNSHostName -eq '$Server'" -Server $GlobalCatalogServer
     } else {
         $oServer = Get-ADComputer -Filter "Name -eq '$Server'" -Server $GlobalCatalogServer
     }
-    #validate the server object exists
+    #validate the server object exists. If the serverobject doesn't exists terminate the function
     if ($null -eq $oServer){
         Write-ScriptMessage -Message "Can't find a server $server in the forest" -Severity Warning -UIused $UIused
         exit
@@ -174,6 +283,11 @@ function New-JITRequestAdminAccess{
         Write-ScriptMessage -Message "Multiple computer found with this name $Server in the current forest. Please use the DNS hostname in stead" -Severity Warning -UIused $UIused
         exit
     }
+    #region build the group name
+    #the group name in mulitdomain mode is
+    #   <AdminPreFix><Domain NetBIOS Name><Seperator><server short name>
+    # in single mode
+    #   <AdminPreFix><Server name>
     if ($config.EnableMultiDomainSupport){
         $ServerDomainDN = [regex]::Match($oserver.DistinguishedName,"DC=.*").value
         $ServerDomainDNSName = (Get-ADForest).domains | Where-Object {(Get-ADDomain -Server $_).DistinguishedName -eq $ServerDomainDN}
@@ -182,11 +296,16 @@ function New-JITRequestAdminAccess{
     } else {
         $ServerGroupName = "$($config.AdminPreFix)$oServer.Name"
     }
-
-    if ($config.EnableMultiDomainSupport){
+    #endregion
+    #if delegation mode is activated, the function validate the user is allowed to request access to this server
+    if ($config.DelegationMode){
+        #reading the delegation repository defined in the JIT configuration
         $DelegationConfig = Get-Content $config.DelegationConfigPath | ConvertFrom-Json
         $userIsAllowed = $false
+        #searching for any DN in the delegation configuration where the server object DN matches
         $DelegationOU = $DelegationConfig | Where-Object {$oServer.distinguishedname -like "*$($_.ComputerOU)"}
+        #searching for matching user SIDs in the server object OUs. On the forst match change the variable userIsAllowed
+        # to $true and stop the loop
         if ($DelegationOU | Where-Object {$_.ADObject -contains $oUser.ObjectSID}){
             $userIsAllowed = $true
         } else {
@@ -197,12 +316,13 @@ function New-JITRequestAdminAccess{
                 }
             }
         }
+        #If a matching delegation can not be found, write a error message and exit the function
         if (!$userIsAllowed){
             Write-ScriptMessage -Message "User is not allowed to request administrator privileges" -Severity Warning -UIused $UIused
             exit
         }
     }
- 
+    #Prepare the eventlog entry and write the JIT request to the Jit eventlog
     $ElevateUser = New-Object PSObject
     $ElevateUser | Add-Member -MemberType NoteProperty -Name "UserDN" -Value $oUser.DistinguishedName
     $ElevateUser | Add-Member -MemberType NoteProperty -Name "ServerGroup" -Value $ServerGroupName
